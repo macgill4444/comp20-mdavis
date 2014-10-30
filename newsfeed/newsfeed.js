959104aaca6468a656ff5b08d0dd6a6dcc585271
Macgill <macgill4444@gmail.com>
2014-10-30 10:21:06 -0400

    first commit

---
+++ newsfeed/newsfeed.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+	//print all old posts
+	for (key in localStorage) {
+		old_post = localStorage[key]; 
+	   $("#news").after(old_post);
+	}
+
+	$('#msg').bind('keyup', function(e) {
+		//if key pressed is enter
+		if(e.keyCode==13){
+		    var post = $(this).val();
+		    date = new Date();
+		    post = "<b>" + post + "</b>" + " - " + date + "<br>";
+		    storePost(post);
+		    $("#news").after(post);
+		    //clear input field
+		    $(this).val("");		    
+		}
+	});
+
+
+
+	function storePost(post) 
+	{
+		if(typeof(Storage) !== "undefined") {
+			localStorage[new Date().getTime()] = post;
+		} else {
+			//no local storage support 
+		}
+	}
+
+
+});
